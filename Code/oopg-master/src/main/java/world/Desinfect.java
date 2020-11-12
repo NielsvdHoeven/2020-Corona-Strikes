c5c26830a8d0f1f2af949b543c6ac4322063cc5f
NielsvdHoeven <nielsvanderhoeven082@Gmail.com>
2020-11-12 08:58:45 +0100

    added enemy class

---
+++ Code/oopg-master/src/main/java/world/Desinfect.java
@@ -0,0 +1,10 @@
+package world;
+
+import nl.han.ica.oopg.objects.Sprite;
+
+public class Desinfect extends Enemy {
+
+    public Desinfect(Sprite sprite) {
+        super(new Sprite());
+    }
+}
---
+++ Code/oopg-master/src/main/java/world/Enemy.java
@@ -0,0 +1,18 @@
+package world;
+
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.objects.SpriteObject;
+
+public class Enemy extends SpriteObject {
+
+    private WorldApp world;
+    
+    public Enemy(WorldApp world) {
+        this.world = world;
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
